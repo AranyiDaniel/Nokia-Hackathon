9211478004ecd58e10508b5ced17c3ac43954c4d
Aranyi Daniel <aranyi.daniel08@gmail.com>
2026-04-16 23:34:55 +0200

    Created first case of next_magic_number

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,9 +1,25 @@
 from pathlib import Path
 
+def next_magic_num(number: int) -> int:
+    numberAsArray = list(map(int, str(number)))
 
+
+    #elso eset
+    numberIsAllNines = False
+    for i in range(len(numberAsArray)):
+        if numberAsArray[i] != 9:
+            break
+        else:
+            numberIsAllNines = True
+    
+    if numberIsAllNines == True:
+        return number + 2
+
+            
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    print(next_magic_num(9999))
+    
 
 
 if __name__ == "__main__":
