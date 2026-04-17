97054ec2479216ed4477486c3dc5011a4d411959
Aranyi Daniel <aranyi.daniel08@gmail.com>
2026-04-17 14:43:42 +0200

    created the second case for next_magic_number function

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -5,22 +5,34 @@ def next_magic_num(number: int) -> int:
 
 
     #elso eset
-    numberIsAllNines = False
     for i in range(len(numberAsArray)):
         if numberAsArray[i] != 9:
             break
         else:
-            numberIsAllNines = True
+            return number + 2
     
-    if numberIsAllNines == True:
-        return number + 2
 
-            
+    #masodik eset
+    lenght = len(numberAsArray)
+    middleIndex = (lenght + 1) // 2
+    leftPart = numberAsArray[:middleIndex]
+    if lenght % 2 == 0:
+        FlippedNumberArray = leftPart + leftPart[::-1]
+    else:
+        FlippedNumberArray = leftPart + leftPart[:-1][::-1]
+
+    FlippedNumber = int("".join(map(str, FlippedNumberArray)))
+    if FlippedNumber > number:
+        return FlippedNumber
+    
+
+
+
+
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(next_magic_num(9999))
-    
-
+    # print(next_magic_num(999))
+    # print(next_magic_num(808))
 
 if __name__ == "__main__":
     main()
